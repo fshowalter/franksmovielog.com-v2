845dbd84f6d187a16c491090ad82a12dd0a8e3e1
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2024-07-29 17:16:13 -0400

    spec

---
+++ src/pages/_index.spec.ts
@@ -0,0 +1,23 @@
+import { getContainerRenderer as reactContainerRenderer } from "@astrojs/react";
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import type { AstroComponentFactory } from "astro/runtime/server/index.js";
+import { loadRenderers } from "astro:container";
+import * as prettier from "prettier";
+import { describe, it } from "vitest";
+
+import Page from "./index.astro";
+
+describe("/", () => {
+  it("matches snapshot", { timeout: 40000 }, async ({ expect }) => {
+    const renderers = await loadRenderers([reactContainerRenderer()]);
+    const container = await AstroContainer.create({ renderers });
+    const result = await container.renderToString(
+      Page as AstroComponentFactory,
+      {},
+    );
+
+    void expect(
+      await prettier.format(result, { parser: "html" }),
+    ).toMatchFileSnapshot(`__snapshots__/index.html`);
+  });
+});
